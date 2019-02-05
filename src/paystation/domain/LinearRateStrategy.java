adab17b5cd0cf00d864efe12dbe1c45f3895a7e2
Jose Silva <tuf06643@temple.edu>
2019-02-04 23:27:05 -0500

    LinearRateStrategy

---
+++ src/paystation/domain/LinearRateStrategy.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paystation.domain;
+
+/**
+ *
+ * Linear Rate Strategy
+ *  Time = (amount * 2)/5
+ * 
+ * @author Silva_Surfer
+ */
+public class LinearRateStrategy implements RateStrategy{
+    
+    public int calculateTime(int moneyInserted){
+        
+        return (moneyInserted * 2)/5;
+        
+    }
+    
+    
+    
+}
