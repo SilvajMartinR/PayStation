511a78293e40b05c5123dbae5ef41baae421f781
Christian Lomboy <christian.lomboy@temple.edu>
2019-02-12 18:28:27 -0500

    Move receipt test methods from PayStationImplTest.java to ReceiptImplTest.java

--- test/paystation/domain/PayStationImplTest.java
+++ test/paystation/domain/PayStationImplTest.java
@@ -100,42 +100,6 @@ public class PayStationImplTest {
     }
 
     /**
-     * Buy should return a valid receipt of the proper amount of parking time
-     */
-    @Test
-    public void shouldReturnCorrectReceiptWhenBuy()
-            throws IllegalCoinException {
-        ps.addPayment(5);
-        ps.addPayment(10);
-        ps.addPayment(25);
-        Receipt receipt;
-        receipt = ps.buy();
-        assertNotNull("Receipt reference cannot be null",
-                receipt);
-        assertEquals("Receipt value must be 16 min.",
-                16, receipt.value());
-    }
-
-    /**
-     * Buy for 100 cents and verify the receipt
-     */
-    @Test
-    public void shouldReturnReceiptWhenBuy100c()
-            throws IllegalCoinException {
-        ps.addPayment(10);
-        ps.addPayment(10);
-        ps.addPayment(10);
-        ps.addPayment(10);
-        ps.addPayment(10);
-        ps.addPayment(25);
-        ps.addPayment(25);
-
-        Receipt receipt;
-        receipt = ps.buy();
-        assertEquals(40, receipt.value());
-    }
-
-    /**
      * Verify that the pay station is cleared after a buy scenario
      */
     @Test
--- test/paystation/domain/ReceiptImplTest.java
+++ test/paystation/domain/ReceiptImplTest.java
@@ -13,5 +13,43 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 
 public class ReceiptImplTest {
-    
+
+    PayStation ps;
+
+    /**
+     * Buy should return a valid receipt of the proper amount of parking time
+     */
+    @Test
+    public void shouldReturnCorrectReceiptWhenBuy()
+            throws IllegalCoinException {
+        ps.addPayment(5);
+        ps.addPayment(10);
+        ps.addPayment(25);
+        Receipt receipt;
+        receipt = ps.buy();
+        assertNotNull("Receipt reference cannot be null",
+                receipt);
+        assertEquals("Receipt value must be 16 min.",
+                16, receipt.value());
+    }
+
+    /**
+     * Buy for 100 cents and verify the receipt
+     */
+    @Test
+    public void shouldReturnReceiptWhenBuy100c()
+            throws IllegalCoinException {
+        ps.addPayment(10);
+        ps.addPayment(10);
+        ps.addPayment(10);
+        ps.addPayment(10);
+        ps.addPayment(10);
+        ps.addPayment(25);
+        ps.addPayment(25);
+
+        Receipt receipt;
+        receipt = ps.buy();
+        assertEquals(40, receipt.value());
+    }
+
 }
