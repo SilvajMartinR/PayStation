ee57b3dd0287dd63dd28fc148eb0fe9626e9daec
Christian Lomboy <christian.lomboy@temple.edu>
2019-02-12 18:21:47 -0500

    Create ReceiptImplTest.java

---
+++ test/paystation/domain/ReceiptImplTest.java
@@ -0,0 +1,17 @@
+/**
+ * Testcases for the Receipt system.
+ *
+ */
+package paystation.domain;
+
+import java.util.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+public class ReceiptImplTest {
+    
+}
